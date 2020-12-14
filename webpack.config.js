fad99a169e1e65739a22e1b8514cac92009d8e49
catse2000 <catse2000@yahoo.com>
2020-12-13 18:58:28 -0600

    added lazy loading, image optimization, and javascript optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: './assets/js/script.js', // enter files that need to be bundled
+module.exports = {// enter files that need to be bundled
+    entry: {
+        app: './assets/js/script.js', 
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: { //tell webpack where bundled files will go
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development' //define what mode this will run in
 };
